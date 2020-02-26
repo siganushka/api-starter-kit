7dc72d6b56467174400346a66cbd1089e7c38de2
siganushka <siganushka@gmail.com>
2020-02-26 21:44:13 +0800

    some changes

--- src/Controller/API/TokenController.php
+++ src/Controller/API/TokenController.php
@@ -7,10 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @apiDefine TokenModel
  *
- * @apiSuccess (Success 200) {String} access_token 访问令牌，需要客户端保存
- * @apiSuccess (Success 200) {String} token_type 令牌类型，暂时仅支持 <code>Bearer</code>
- * @apiSuccess (Success 200) {String} expires_in 访问令牌有效期，过期后需要客户端主动刷新
- * @apiSuccess (Success 200) {String} refresh_token 刷新令牌，需要客户端保存，有效期固定 1 个月
+ * @apiSuccess (Success 2xx) {String} access_token 访问令牌，需要客户端保存
+ * @apiSuccess (Success 2xx) {String} token_type 令牌类型，暂时仅支持 <code>Bearer</code>
+ * @apiSuccess (Success 2xx) {Number} expires_in 访问令牌有效期，过期后需要客户端主动刷新，单位：秒
+ * @apiSuccess (Success 2xx) {String} refresh_token 刷新令牌，用于换取新的访问令牌，需要客户端保存，有效期暂定 <code>1</code> 个月
  */
 class TokenController extends AbstractController
 {
@@ -19,13 +19,14 @@ class TokenController extends AbstractController
      *
      * @api {post} /token 1、获取认证令牌
      *
-     * @apiGroup Token
+     * @apiGroup 帐户
      * @apiVersion 0.1.0
      *
      * @apiParam (Body) {String} username 用户登录名
      * @apiParam (Body) {String} password 用户登录密码
      *
      * @apiUse TokenModel
+     * @apiUse Error
      */
     public function token()
     {
@@ -37,12 +38,13 @@ class TokenController extends AbstractController
      *
      * @api {post} /refresh_token 2、刷新认证令牌
      *
-     * @apiGroup Token
+     * @apiGroup 帐户
      * @apiVersion 0.1.0
      *
      * @apiParam (Body) {String} refresh_token 刷新令牌
      *
      * @apiUse TokenModel
+     * @apiUse Error
      */
     public function refreshToken()
     {
--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -9,23 +9,24 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @apiDefine UserModel
  *
- * @apiSuccess (Success 200) {String} id 用户 ID
- * @apiSuccess (Success 200) {String} username 用户名（唯一标识）
- * @apiSuccess (Success 200) {String} avatar 用户头像
- * @apiSuccess (Success 200) {String} updated_at 最后更新时间
- * @apiSuccess (Success 200) {String} created_at 用户创建时间
+ * @apiSuccess (Success 2xx) {String} id 用户 ID
+ * @apiSuccess (Success 2xx) {String} username 用户名（唯一标识）
+ * @apiSuccess (Success 2xx) {String} avatar 用户头像
+ * @apiSuccess (Success 2xx) {String} updated_at 最后更新时间
+ * @apiSuccess (Success 2xx) {String} created_at 用户创建时间
  */
 class UserController extends AbstractController
 {
     /**
      * @Route("/users/current", name="api_users_current", methods={"GET"})
      *
-     * @api {get} /users/current 1、获取当前用户信息
+     * @api {get} /users/current 3、获取当前用户信息
      *
-     * @apiGroup User
+     * @apiGroup 帐户
      * @apiVersion 0.1.0
      *
      * @apiUse UserModel
+     * @apiUse Error
      *
      * @return Response
      */
